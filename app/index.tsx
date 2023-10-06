8e7b2b8e09a11ec81b97fb6262ffe456733ee98c
Rajesh <rajcreates@zohomail.in>
2023-10-06 15:51:45 +0530

    Initial Commit

---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import { Image, StyleSheet } from "react-native";
+import { Text, View } from "../components/Themed";
+
+export default function HomeScreen() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Restaurant App!!!</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+      }
+})
---
+++ components/FoodItem.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet} from "react-native";
+import { Text, View } from "./Themed";
+
+export default function FoodItem() {
+    <View>
+        <Text>Food App</Text>
+    </View>
+}
+
+const styles = StyleSheet.create({
+    image: {
+        width: 250,
+        height: 150
+    }
+})
