972cc47a28b6d6c40ac4cf06eb2165a102ad9d86
Rajesh <rajcreates@zohomail.in>
2023-10-07 12:31:54 +0530

    Made food items to display on homepage

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,55 @@
-import { Image, StyleSheet } from "react-native";
-import { Text, View } from "../components/Themed";
+import React, { useState } from 'react';
+import { FlatList, SafeAreaView, StyleSheet } from 'react-native';
+import { TextInput } from '../components/Themed';
+import FoodItem from '../components/FoodItem';
+import foodData from '../data.json';
 
-export default function HomeScreen() {
+const HomeScreen = () => {
+    const [search, setSearch] = useState('');
+    const [filter, setFilter] = useState('price');
+
+    const filteredData = foodData.filter(item =>
+        item.name.toLowerCase().includes(search.toLowerCase()) ||
+        item.description.toLowerCase().includes(search.toLowerCase())
+    ).sort((a, b) => 
+        filter === 'price' ? a.price - b.price : a.name.localeCompare(b.name)
+    );
+
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Restaurant App!!!</Text>
-        </View>
-    )
-}
+        <SafeAreaView style={styles.container}>
+            <TextInput 
+                value={search}
+                onChangeText={setSearch}
+                placeholder="Search for food"
+                style={styles.textInput}
+            />
+            {/* ...other UI elements like filter dropdown */}
+            <FlatList
+                data={filteredData}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item }) => (
+                    <FoodItem item={item} onPress={(id) => console.log('Navigate to Details with', id)} />
+                )}
+            />
+        </SafeAreaView>
+    );
+};
+
+export default HomeScreen;
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
+        color: 'white'
       },
-      title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-      }
+    textInput: {
+        height: 40, 
+        borderColor: 'gray', 
+        borderWidth: 1, 
+        marginBottom: 16,
+        borderRadius: 16,
+        padding:16
+    }
 })
--- components/FoodItem.tsx
+++ components/FoodItem.tsx
@@ -1,15 +1,42 @@
-import { StyleSheet} from "react-native";
-import { Text, View } from "./Themed";
+import React from 'react';
+import { Image, TouchableOpacity, StyleSheet } from 'react-native';
+import { Text, View } from './Themed';
 
-export default function FoodItem() {
-    <View>
-        <Text>Food App</Text>
-    </View>
+interface FoodItemProps {
+    item: {
+        id: string;
+        name: string;
+        description: string;
+        price: number;
+        image: string;
+    };
+    onPress: (id: string) => void;
 }
 
+const FoodItem: React.FC<FoodItemProps> = ({ item, onPress }) => {
+    return (
+        <TouchableOpacity onPress={() => onPress(item.id)}>
+            <View style={{ marginBottom: 16 }}>
+                <Image source={{ uri: item.image }} style={styles.imgContainer} />
+                <Text style={styles.title}>{item.name}</Text>
+                <Text>{item.description}</Text>
+                <Text>${item.price}</Text>
+            </View>
+        </TouchableOpacity>
+    );
+};
+
+export default FoodItem;
+
 const styles = StyleSheet.create({
-    image: {
-        width: 250,
-        height: 150
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'white'
+      },
+    imgContainer: {
+        width: 200,
+        height: 200,
+        borderRadius: 16
     }
 })
